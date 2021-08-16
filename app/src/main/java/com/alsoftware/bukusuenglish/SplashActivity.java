5f4e19702967a02b6937f666edfb3c9402d41918
Lusengeri <lusengeri@gmail.com>
2021-08-16 16:17:09 +0300

    Transfer icons from version specific folder

--- app/src/main/java/com/alsoftware/bukusuenglish/SplashActivity.java
+++ app/src/main/java/com/alsoftware/bukusuenglish/SplashActivity.java
@@ -8,6 +8,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+
 public class SplashActivity extends AppCompatActivity {
     Animation animation;
     ImageView imageView;
